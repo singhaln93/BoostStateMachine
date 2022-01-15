ad060619663bc016f917222dfa161dec971393e1
Neeraj Singhal <Neerajsinghal93@gmail.com>
2022-01-15 12:30:15 +0100

    Updated formatting cmake files and added ccache.cmake

--- scripts/cmake/catch2Test.cmake
+++ scripts/cmake/catch2Test.cmake
@@ -1,11 +1,10 @@
 find_package(Catch2 REQUIRED)
 
 # file(GLOB_RECURSE TEST_SOURCES "test/*.cpp")
-# add_test(NAME TEST_RELEASE_ADD  COMMAND [Release] ../../test/addTest.cpp)
+add_test(NAME TEST_LIB_ADD COMMAND ctest -T)
 
-# add_executable(tests ${TEST_SOURCES})
-# target_link_libraries(tests PRIVATE Catch2::Catch2)
-# catch_discover_tests(tests)
+# add_executable(tests ${TEST_SOURCES}) target_link_libraries(tests PRIVATE
+# Catch2::Catch2) catch_discover_tests(tests)
 
 include(CTest)
 enable_testing()
---
+++ scripts/cmake/ccache.cmake
@@ -0,0 +1,7 @@
+find_program(CCACHE ccache)
+if(CCACHE)
+  message("-- using ccache")
+  set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+else()
+  message("ccache not found cannot use")
+endif()
